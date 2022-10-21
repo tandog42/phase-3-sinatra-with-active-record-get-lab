02aaa29f44ec9f3aee53af595ee29d3015b7ce7e
Tanner <troxs690@gmail.com>
2022-10-21 15:37:57 -0500

    d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
+
+    get '/bakeries' do
+      bakery = Bakery.all
+      bakery.to_json
+    end
+
+    get '/bakeries/:id' do
+      bake = Bakery.find(params[:id])
+      bake.to_json(include: :baked_goods)
+    end
+  
+    get '/baked_goods/by_price' do
+    prices = BakedGood.order("price DESC" )
+    prices.to_json
+  end
+  get '/baked_goods/most_expensive' do
+    baked = BakedGood.order("price DESC").first
+    baked.to_json
+  end
 
 end
